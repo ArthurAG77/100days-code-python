60c672e8bca8d227fa2faf494015553f4f6a36e3
ArthurAG77 <arthur.ag77@gmail.com>
2024-12-07 00:53:09 -0300

    i'm tired going to the bed that's my last one

---
+++ day 01/Factorial number/factorial.py
@@ -0,0 +1,18 @@
+"""Write a function to calculate the factorial of a number."""
+from time import sleep
+
+def factorial(num):
+    calc = []
+    fact = 1
+
+    for i in range(num, 0, -1):
+        calc.append(i)
+
+    for i in calc:
+        fact *= i
+        print(f"{i} x ", end="", flush=True)
+        sleep(.2)
+    print(f" = {fact}", flush=True)
+
+if __name__ == "__main__":
+    factorial(11)
