208d2d2259fcd44b818a1529fcc2c75f0e736250
ArthurAG77 <arthur.ag77@gmail.com>
2024-12-07 23:13:48 -0300

    my last one today, i tried to make this more simple tan possible

---
+++ Day 02/Using lists/longest_word_in_sentence.py
@@ -0,0 +1,9 @@
+def find_longest_word(sentence:str):
+    words_list = sentence.split()
+    longest_word = ''
+    for word in words_list:
+        if len(longest_word) < len(word):
+            longest_word = word
+    print(longest_word)
+
+find_longest_word("O rato roeu a roupa do rei de roma")
